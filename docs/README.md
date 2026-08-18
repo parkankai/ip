e8d268d290351353f5386c753487667abb12d068
park <254471492+parkankai@users.noreply.github.com>
2026-08-18 18:25:49 +0800

    Duke level0 -> changed all references to doe, changed banner and intro text

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# doe User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/doe.java
@@ -0,0 +1,20 @@
+/**
+ * Entry point for the doe chatbot application.
+ */
+public class doe {
+    public static void main(String[] args) {
+        String banner = "____________________________________________________________\n"
+                + "     _          \n"
+                + "  __| | ___  ___\n"
+                + " / _` |/ _ \\ / _ \\\n"
+                + "| (_| | (_) |  __/\n"
+                + " \\__,_|\\___/ \\___|\n"
+
+                + "Hello! I'm doe :).\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(banner);
+    }
+}
