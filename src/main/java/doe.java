7798b261bb5886900fd40a1cb34ce476e1cca9d8
park <254471492+parkankai@users.noreply.github.com>
2026-08-18 19:08:27 +0800

    duke level1 -> gave options and replies and exit

--- src/main/java/doe.java
+++ src/main/java/doe.java
@@ -1,8 +1,14 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  * Entry point for the doe chatbot application.
  */
+import java.util.Scanner;
 public class doe {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String banner = "____________________________________________________________\n"
                 + "     _          \n"
                 + "  __| | ___  ___\n"
@@ -10,11 +16,61 @@ public class doe {
                 + "| (_| | (_) |  __/\n"
                 + " \\__,_|\\___/ \\___|\n"
 
-                + "Hello! I'm doe :).\n"
-                + "What can I do for you?\n"
+                + "hello! i'm doe :).\n"
+                + "what can i do for you?\n"
                 + "____________________________________________________________\n"
+                + "1. neigh\n"
+                + "2. meow\n"
+                + "3. list\n"
+                + "____________________________________________________________\n"
+                + "type \"bye\" to exit\n"
+                + "____________________________________________________________\n";
+
+        String neigh = "____________________________________________________________\n"
+                + "EURHGGGHHHHH!\n"
+                + "____________________________________________________________\n";
+
+        String meow = "____________________________________________________________\n"
+                + "meow!\n"
+                + "____________________________________________________________\n";
+
+        String list = "____________________________________________________________\n"
+                + "roles and responsiblities\n"
+                + "1. survive nus cs\n"
+                + "2. get a few internships\n"
+                + "3. work at mcdonalds\n"
+                + "4. retire as a manager(hopefully)\n"
+                + "____________________________________________________________\n";
+
+        String bye = "____________________________________________________________\n"
                 + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
+
         System.out.println(banner);
+        while(true){
+            String input = scanner.nextLine().toLowerCase();
+            switch (input) {
+                case "neigh":
+                    System.out.println(neigh);
+                    break;
+                case "meow":
+                    System.out.println(meow);
+                    break;
+                case "list":
+                    System.out.println(list);
+                    break;
+                case "bye":
+                    System.out.println(bye);
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("horh");
+
+            }
+        }
+
+
+
+
     }
 }
